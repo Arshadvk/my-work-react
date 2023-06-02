70c83bbc5328c942e89fdbc7cf1d5454fa89f248
Arshadvk <arshadvk7560@gmail.com>
2023-06-02 16:59:36 +0530

    pratice react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route , Routes , Link } from 'react-router-dom'
+import About from './container/about';
+import Profile from './container/profile';
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to='/about'>about</Link>
+      <Link to='/profile'>profile</Link>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+
     </div>
   );
 }
